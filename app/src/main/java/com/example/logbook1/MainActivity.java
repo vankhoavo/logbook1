15b1696aaad1100de2daa49e3abc7247c5aca289
VoVanKhoa <vovankhoa2001@hotmail.com>
2023-10-08 22:54:35 +0700

    logbook1

--- app/src/main/java/com/example/logbook1/MainActivity.java
+++ app/src/main/java/com/example/logbook1/MainActivity.java
@@ -1,100 +1,227 @@
 package com.example.logbook1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.material.button.MaterialButton;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
-    TextView resultcal, solutioncal;
-    MaterialButton button_c, button_close_bracket, button_open_bracket, button_div;
-    MaterialButton button_1,button_2,button_3,button_4,button_5,button_6,button_7,button_8,button_9,button_zero;
-    MaterialButton button_mul, button_sum, button_sub, button_equal,button_ac,button_dot;
+    private TextView resultTextView;
+    private TextView solutionTextView;
+
+    private StringBuilder currentInput;
+    private double currentResult;
+    private String currentOperator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        init();
+        resultTextView = findViewById(R.id.result);
+        solutionTextView = findViewById(R.id.solution);
+
+        currentInput = new StringBuilder();
+        currentResult = 0;
+        currentOperator = "";
+
+        setNumberButtonListeners();
+        setOperatorButtonListener();
+        setOtherButtonListeners();
     }
-     public void init()
-     {
-         // Kết nối code và đặt id cho từng button hoặc từng text view
-         resultcal = (TextView)findViewById(R.id.result);
-         solutioncal = (TextView)findViewById(R.id.solution);
-         button_c = (MaterialButton)findViewById(R.id.button_c);
-         button_close_bracket = (MaterialButton)findViewById(R.id.button_close_bracket);
-         button_open_bracket = (MaterialButton)findViewById(R.id.button_open_bracket);
-         button_div = (MaterialButton)findViewById(R.id.button_div);
-         button_1 = (MaterialButton)findViewById(R.id.button_1);
-         button_2 = (MaterialButton)findViewById(R.id.button_2);
-         button_3 = (MaterialButton)findViewById(R.id.button_3);
-         button_4 = (MaterialButton)findViewById(R.id.button_4);
-         button_5 = (MaterialButton)findViewById(R.id.button_5);
-         button_6 = (MaterialButton)findViewById(R.id.button_6);
-         button_7 = (MaterialButton)findViewById(R.id.button_7);
-         button_8 = (MaterialButton)findViewById(R.id.button_8);
-         button_9 = (MaterialButton)findViewById(R.id.button_9);
-         button_zero = (MaterialButton)findViewById(R.id.button_zero);
-         button_mul = (MaterialButton)findViewById(R.id.button_mul);
-         button_sum = (MaterialButton)findViewById(R.id.button_sum);
-         button_sub = (MaterialButton)findViewById(R.id.button_sub);
-         button_equal = (MaterialButton)findViewById(R.id.button_equal);
-         button_ac = (MaterialButton)findViewById(R.id.button_ac);
-         button_dot = (MaterialButton)findViewById(R.id.button_dot);
 
-         //Set click button with setOnClickListener
-         button_1.setOnClickListener(MainActivity.this);
-         button_c.setOnClickListener(this);
-         button_close_bracket.setOnClickListener(this);
-         button_open_bracket.setOnClickListener(this);
-         button_div.setOnClickListener(this);
-         button_2.setOnClickListener(this);
-         button_3.setOnClickListener(this);
-         button_4.setOnClickListener(this);
-         button_5.setOnClickListener(this);
-         button_6.setOnClickListener(this);
-         button_7.setOnClickListener(this);
-         button_8.setOnClickListener(this);
-         button_9.setOnClickListener(this);
-         button_zero.setOnClickListener(this);
-         button_mul.setOnClickListener(this);
-         button_sum.setOnClickListener(this);
-         button_sub.setOnClickListener(this);
-         button_equal.setOnClickListener(this);
-         button_ac.setOnClickListener(this);
-         button_dot.setOnClickListener(this);
-     }
+    private void setNumberButtonListeners() {
+        Button button_zero = findViewById(R.id.button_zero);
+        Button button_1 = findViewById(R.id.button_1);
+        Button button_2 = findViewById(R.id.button_2);
+        Button button_3 = findViewById(R.id.button_3);
+        Button button_4 = findViewById(R.id.button_4);
+        Button button_5 = findViewById(R.id.button_5);
+        Button button_6 = findViewById(R.id.button_6);
+        Button button_7 = findViewById(R.id.button_7);
+        Button button_8 = findViewById(R.id.button_8);
+        Button button_9 = findViewById(R.id.button_9);
+        Button button_dot = findViewById(R.id.button_dot);
+        button_zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClick(button_zero.getText().toString());
+            }
+        });
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClick(button_1.getText().toString());
+            }
+        });
+        button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClick(button_2.getText().toString());
+            }
+        });
+        button_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClick(button_3.getText().toString());
+            }
+        });
+        button_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClick(button_4.getText().toString());
+            }
+        });
+        button_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClick(button_5.getText().toString());
+            }
+        });
+        button_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClick(button_6.getText().toString());
+            }
+        });
+        button_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClick(button_7.getText().toString());
+            }
+        });
+        button_8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClick(button_8.getText().toString());
+            }
+        });
+        button_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClick(button_9.getText().toString());
+            }
+        });
+        button_dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClick(button_dot.getText().toString());
+            }
+        });
+    }
 
-    @Override
-    public void onClick(View view) {
-        MaterialButton button = (MaterialButton) view;
-        // Lấy chỗ activity_main android:text="AC"
-        String buttonText = button.getText().toString();
-        String datacal = solutioncal.getText().toString();
+    private void setOperatorButtonListener() {
+        Button button_sum = findViewById(R.id.button_sum);
+        Button button_sub = findViewById(R.id.button_sub);
+        Button button_mul = findViewById(R.id.button_mul);
+        Button button_div = findViewById(R.id.button_div);
+        button_sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleOperatorButtonClick(button_sum.getText().toString());
+            }
+        });
+        button_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleOperatorButtonClick(button_sub.getText().toString());
+            }
+        });
+        button_mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleOperatorButtonClick(button_mul.getText().toString());
+            }
+        });
+        button_div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleOperatorButtonClick(button_div.getText().toString());
+            }
+        });
+    }
 
-        if(buttonText.equals("AC")){
-            solutioncal.setText("");
-            resultcal.setText("0");
-            return;
-        }
+    private void setOtherButtonListeners() {
+        Button buttonEqual = findViewById(R.id.button_equal);
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleEqualButtonClick();
+            }
+        });
 
-        if(buttonText.equals("=")){
-            solutioncal.setText(resultcal.getText());
-            return;
-        }
+        Button buttonAC = findViewById(R.id.button_ac);
+        buttonAC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleACButtonClick();
+            }
+        });
+    }
 
-        if(buttonText.equals("C")){
-            datacal = datacal.substring(0,datacal.length()-1);
-        }else {
-            datacal = datacal + buttonText;
-        }
+    private void handleNumberButtonClick(String number) {
+        currentInput.append(number);
+        updateResultTextView();
+    }
 
-        solutioncal.setText(datacal);
+    private void handleOperatorButtonClick(String operator) {
+        if (currentInput.length() > 0) {
+            currentResult = Double.parseDouble(currentInput.toString());
+            currentOperator = operator;
+            currentInput.setLength(0);
+            updateSolutionTextView();
         }
     }
+
+    private void handleEqualButtonClick() {
+        if (currentInput.length() > 0 && !currentOperator.isEmpty()) {
+            double operand = Double.parseDouble(currentInput.toString());
+            switch (currentOperator) {
+                case "+":
+                    currentResult = currentResult + operand;
+                    break;
+                case "-":
+                    currentResult = currentResult - operand;
+                    break;
+                case "*":
+                    currentResult = currentResult * operand;
+                    break;
+                case "/":
+                    if (operand != 0) {
+                        currentResult = currentResult / operand;
+                    } else {
+                        // Handle division by zero
+                        currentResult = 0;
+                    }
+                    break;
+            }
+            currentInput.setLength(0);
+            updateResultTextView();
+            updateSolutionTextView();
+        }
+    }
+
+    private void handleACButtonClick() {
+        currentInput.setLength(0);
+        currentResult = 0;
+        currentOperator = "";
+        updateResultTextView();
+        updateSolutionTextView();
+    }
+
+    private void updateResultTextView() {
+        resultTextView.setText(currentInput.toString());
+    }
+
+    private void updateSolutionTextView() {
+        int intResult = (int) currentResult; // Lấy phần nguyên của kết quả
+        if (currentResult - intResult == 0) {
+            solutionTextView.setText(String.valueOf(intResult));
+        } else {
+            solutionTextView.setText(String.format("%.2f %s", currentResult, currentOperator));
+        }
+    }
+}
